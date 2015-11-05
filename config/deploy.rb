2bd00f293c39bd46510e199bdd6b6a0584668cf7
alex@xubuntu_vm <alex@ubuntu_vm>
2015-11-05 02:43:14 -0800

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,8 +30,7 @@ namespace :deploy do
   task :stop do ; end
 
   task :start do
-    #run "passenger start /var/www/yellowings_mc/current/ -a 151.236.10.206 -p 4000 -d -e production"
-
+    run "passenger start /var/www/yellowings_mc/current/ -a 151.236.10.206 -p 4000 -d -e production"
   end
 
 
@@ -51,7 +50,7 @@ end
 
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      #execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
