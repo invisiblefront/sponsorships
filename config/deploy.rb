b83d04e3489664340c56a2724ea5d75454a00bbd
alex@xubuntu_vm <alex@ubuntu_vm>
2015-11-05 02:44:10 -0800

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ namespace :deploy do
   task :stop do ; end
 
   task :start do
+    #run "$(sudo lsof -t -i:4000)"
     run "passenger start /var/www/yellowings_mc/current/ -a 151.236.10.206 -p 4000 -d -e production"
   end
 
@@ -50,7 +51,7 @@ end
 
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      run "passenger start /var/www/yellowings_mc/current/ -a 151.236.10.206 -p 4000 -d -e production"
     end
   end
 
