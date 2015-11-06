9c666cd12ad527ca18daff266f32dce6d91b82d8
Alex Windows WTF <al77ex@gmail.com>
2015-11-06 21:35:30 +0100

    can see only my sponsors as organizer

--- app/controllers/organizers/sponsors_controller.rb
+++ app/controllers/organizers/sponsors_controller.rb
@@ -1,7 +1,8 @@
 module Organizers
   class SponsorsController < ApplicationController
     def index
-      @my_sponsors=Sponsor.all
+      @project_id=current_organizer.project.id
+      @my_sponsors=Sponsor.joins(:project_sponsorships).where(:project_sponsorships => {:project_id => @project_id})
     end
   end
 end
